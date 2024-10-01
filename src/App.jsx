d47f83a3097a0d38baec0bcdbce07593980b363f
Alejandro Diaz <alejandroidb@gmail.com>
2024-10-01 15:58:26 -0300

    modificacion tamaño y estilos vistas

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import { Routes, Route } from 'react-router-dom' 
+import { Routes, Route } from "react-router-dom";
 
 //COMPONENTS
-import NavBar from './components/NavBar'
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 //VIEWS
-import HomePage from './views/HomePage'
-import Footer from './components/Footer'
+import HomePage from "./views/HomePage";
+import RegisterPage from "./views/RegisterPage";
 
 //CSS
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      <NavBar/>
+    <div className="App">
+      <NavBar />
+      <div className="content">
       <Routes>
-        <Route
-          path = '/' 
-          element = {<HomePage/>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<RegisterPage />} />
       </Routes>
-      <Footer/>
-    </>
-  )
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="d-flex flex-column min-vh-100">
-
+        <div className="d-flex flex-column">
             <footer className="bg-dark text-white py-4 mt-auto">
                 <p className="text-center m-0">
                     &copy; {new Date().getFullYear()} Game Store. All rights reserved.
