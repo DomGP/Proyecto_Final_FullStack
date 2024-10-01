7d8afc19b4a0f32ea58be300bcc4db0030895db0
Alejandro Diaz <alejandroidb@gmail.com>
2024-10-01 11:39:34 -0300

    arreglo footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-dark text-white py-4">
-                <p>&copy; {new Date().getFullYear()} Game Store. All rights reserved.</p>        
-        </footer>
+        <div className="d-flex flex-column min-vh-100">
+
+            <footer className="bg-dark text-white py-4 mt-auto">
+                <p className="text-center m-0">
+                    &copy; {new Date().getFullYear()} Game Store. All rights reserved.
+                </p>
+            </footer>
+        </div>
     );
 };
 
