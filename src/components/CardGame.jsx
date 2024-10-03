6ba4d0da0a73593563ca72d055f0b94207372160
Alejandro Diaz <alejandroidb@gmail.com>
2024-10-03 11:45:21 -0300

    correccion estilos tarjetas

--- src/components/CardGame.jsx
+++ src/components/CardGame.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { Container, Row, Col } from "react-bootstrap";
 
 const CardGame = ({ showDetails = true }) => {
   const [productos, setProductos] = useState([]);
@@ -13,22 +14,30 @@ const CardGame = ({ showDetails = true }) => {
   }, []);
 
   return (
-    <div className="row">
-      {productos.map((producto) => (
-        <div className="col-md-4" key={producto.id}>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={producto.img_url} alt={producto.nombre} />
-            {showDetails && (
-              <Card.Body>
-                <Card.Title>{producto.nombre}</Card.Title>
-                <Card.Text>{producto.descripcion}</Card.Text>
-                <Button variant="primary">Comprar por ${producto.precio}</Button>
-              </Card.Body>
-            )}
-          </Card>
-        </div>
-      ))}
-    </div>
+    <Container>
+      <Row className="justify-content-center">
+        {productos.map((producto) => (
+          <Col xs={12} sm={6} md={4} lg={3} className="d-flex justify-content-center mb-4" key={producto.id}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img 
+                variant="top" 
+                src={producto.img_url} 
+                alt={producto.nombre} 
+                className="img-fluid"
+                style={{ height: "300px", objectFit: "fill" }} 
+              />
+              {showDetails && (
+                <Card.Body>
+                  <Card.Title>{producto.nombre}</Card.Title>
+                  <Card.Text>{producto.descripcion}</Card.Text>
+                  <Button variant="primary">Comprar por ${producto.precio}</Button>
+                </Card.Body>
+              )}
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
 
