57bde71586efc997af576b7545c96473c244d7de
Alejandro Diaz <alejandroidb@gmail.com>
2024-10-03 10:14:06 -0300

    correcciones menores

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -6,9 +6,9 @@ const RegisterForm = () => {
     <div>
       <div className="container my-5">
         <div className="row justify-content-center">
-          <div className="col-12 col-md-9 col-lg-7 col-xl-6 col-xxl-5 w-75 mx-auto">
+          <div className="col-12 col-md-9 col-lg-7 col-xl-6 col-xxl-5">
             <div className="card border border-light-subtle rounded-4">
-              <div className="card-body p-3 p-md-4 p-xl-5">
+              <div className="card-body p-3 pt-2 p-md-4 p-xl-5">
                 <div className="row">
                   <div className="col-12">
                     <div className="mb-5">
